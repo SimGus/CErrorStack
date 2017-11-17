7a7905f465d2f1498242fc6821be05d915625e69
sigustin <simon.gustin@student.uclouvain.be>
2017-11-17 02:01:32 +0100

    Initial commit for defines

---
+++ src/defines.h
@@ -0,0 +1,10 @@
+#ifndef _CERRORSTACK_DEFINES_H_
+#define _CERRORSTACK_DEFINES_H_
+
+#include <stdbool.h>
+
+#define SAFE   1
+
+#define DEBUG(msg)   printf("DEBUG:\t%s\n", msg);
+
+#endif //_CERRORSTACK_DEFINES_H_
